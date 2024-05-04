763053112f15704584e97f0d0fa0b1619e59b868
Subarna Bajracharya <subarna.bajracharya@patancollege.edu.np>
2024-05-04 14:48:03 +0545

    Frontend Finished

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template('index.html')
     # return 'Hello, World!'
 
+
+@app.route('/contactus/')
+def contactus():
+    return render_template('contactus.html')
+
+
+@app.route('/courses/')
+def courses():
+    return render_template('course.html')
+
+
+@app.route('/aboutus/')
+def aboutus():
+    return render_template('aboutus.html')
+
+
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
+
+    
